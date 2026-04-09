ca61315fbffe07b3c5c3739ab792843e039f6294
CriSTEM <cristianagguniversity@gmail.com>
2026-04-09 10:52:25 -0600

    feat: add why-stuck diagnosis workflow service

--- src/memops/services/__init__.py
+++ src/memops/services/__init__.py
@@ -1,16 +1,19 @@
 """Core service layer for MemOps."""
 
 from .analysis import TransactionAnalysis, analyze_parsed_transaction
+from .diagnosis import DiagnosedTransaction, diagnose_why_stuck
 from .inspection import InspectedTransaction, inspect_transaction
 from .policy import any_input_signals_explicit_rbf, signals_explicit_rbf
 from .tx_parser import ParsedTransaction, parse_raw_transaction
 
 __all__ = [
+    "DiagnosedTransaction",
     "InspectedTransaction",
     "ParsedTransaction",
     "TransactionAnalysis",
     "analyze_parsed_transaction",
     "any_input_signals_explicit_rbf",
+    "diagnose_why_stuck",
     "inspect_transaction",
     "parse_raw_transaction",
     "signals_explicit_rbf",
